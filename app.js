0527515af3cb173d4933ed2039cd81a13183484d
Valentin Himself <valentinhimself@gmail.com>
2022-12-23 21:18:01 -0700

    Add basic bot functionality

--- app.js
+++ app.js
@@ -2,24 +2,33 @@ class TicTacToe {
   constructor(tick, tac) {
     this.tick = tick;
     this.tac = tac;
-    this.Array = [];
+    this.Array = new Array(9);
   }
 
   setPlayerChoice(choice) {
     this.playerChoice = '';
-    if (choice === this.tick) this.playerChoice = this.tick;
-    if (choice === this.tac) this.playerChoice = this.tac;
-  }
-
-  takeTurn() {
-    if (!this.playerChoice) return;
-
-    if (this.playerChoice === this.tac) {
+    this.botChoice = '';
+    if (choice === this.tick) {
       this.playerChoice = this.tick;
-    } else {
+      this.botChoice = this.tac;
+    }
+    if (choice === this.tac) {
       this.playerChoice = this.tac;
+      this.botChoice = this.tick;
+      this.botMove();
+      this.updateDisplay();
     }
   }
+
+  // takeTurn() {
+  //   if (!this.playerChoice) return;
+
+  //   if (this.playerChoice === this.tac) {
+  //     this.playerChoice = this.tick;
+  //   } else {
+  //     this.playerChoice = this.tac;
+  //   }
+  // }
 
   isCellTaken(cell) {
     this.currentCell = cell.textContent;
@@ -28,12 +37,43 @@ class TicTacToe {
 
   makeMove(cell) {
     if (!this.Array[0]) this.Array[0] = null; // debug
+    if (this.playerChoice === '') this.playerChoice = 'X';
     this.Array[Array.from(gameController.tickTacFields).indexOf(cell)] =
       this.playerChoice;
   }
 
-  updateDisplay(cell) {
-    cell.textContent = this.playerChoice;
+  botMove() {
+    if (!this.Array[4] && this.botChoice === this.tac) {
+      this.Array[4] = this.botChoice;
+      return;
+    }
+
+    const availableArrayIndices = this.getAvailableIndices();
+
+    this.randomIndexFromRemaining =
+      availableArrayIndices[
+        Math.floor(Math.random() * availableArrayIndices.length)
+      ];
+
+    this.Array[this.randomIndexFromRemaining] = this.botChoice;
+  }
+
+  getAvailableIndices() {
+    const availableArrayIndices = [];
+    for (let i = 0; i < this.Array.length; i++) {
+      if (!this.Array[i]) {
+        availableArrayIndices.push(i);
+      }
+    }
+    return availableArrayIndices;
+  }
+
+  updateDisplay() {
+    let i = 0;
+    while (i < this.Array.length) {
+      gameController.tickTacFields[i].textContent = this.Array[i];
+      i += 1;
+    }
     if (!this.isOver()) return;
     gameController.declareWinner();
   }
@@ -42,7 +82,7 @@ class TicTacToe {
     gameController.overlay.classList.remove('show');
     this.playerChoice = '';
     gameController.tickTacFields.forEach((field) => (field.textContent = ''));
-    this.Array.length = 0;
+    this.Array = new Array(9);
   }
 
   isOver() {
@@ -60,13 +100,14 @@ class TicTacToe {
     if (this.Array[2] === this.Array[4] && this.Array[2] === this.Array[6])
       return this.Array[2];
 
-    // vertical
+    // // vertical
     if (this.Array[0] === this.Array[3] && this.Array[3] === this.Array[6])
       return this.Array[0];
     if (this.Array[1] === this.Array[4] && this.Array[4] === this.Array[7])
       return this.Array[1];
     if (this.Array[2] === this.Array[5] && this.Array[5] === this.Array[8])
       return this.Array[2];
+
     if (!this.Array.includes(undefined)) return 'Draw';
   }
 }
@@ -93,9 +134,13 @@ function GameController() {
       tickTacField.addEventListener('click', () => {
         if (!tickTac.isCellTaken(tickTacField)) {
           tickTac.makeMove(tickTacField);
-          tickTac.updateDisplay(tickTacField);
-          tickTac.isOver();
-          tickTac.takeTurn();
+          tickTac.updateDisplay();
+          setTimeout(() => {
+            tickTac.botMove();
+            tickTac.updateDisplay();
+            tickTac.isOver();
+          }, 500);
+          // tickTac.takeTurn();
         }
       });
     });
